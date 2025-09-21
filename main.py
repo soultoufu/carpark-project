5f75a194150276cd3957adc5be2f73a950985c2d
soultoufu <179322367+soultoufu@users.noreply.github.com>
2025-09-21 21:34:07 +0800

    Add end point /carparks

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Didi took an L"}
+
+@app.get("/carparks")
+async def get_carparks():
+    return [
+        {"id": 1, "name": "HDB Lot A", "price": 1.20, "availability": 35},
+        {"id": 2, "name": "HDB Lot B", "price": 2.00, "availability": 32}
+    ]
